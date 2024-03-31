43281e09e1ffb037268c96ea3bc563d8616127e4
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-04-01 02:14:42 +0500

    project 2

---
+++ index.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+//1 campare will generate a random number
+//2 user input for guessing number
+//3 campare user input with computer generated number and show result
+
+const randomNumber = Math.floor(Math.random() * 9 + 1);
+const answers = await inquirer.prompt([
+    {
+        name:"userGuessedNumber",
+        type:"number",
+        message:"please guess a number between 1-10:",
+    }
+]);
+if(answers.userGuessedNumber === randomNumber){
+    console.log("Congratulation!you gussed right number");
+}else{
+    console.log("sorry! you guessed wrong number");
+}
